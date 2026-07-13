f7923c81a0a521610ba8d9ecef1341cfd27570ed
Jaweriagul <jaweriagul2009@gmail.com>
2026-07-13 10:14:39 -0700

    py

---
+++ python/parrot bird.py
@@ -0,0 +1,15 @@
+class parrot:
+    specie = "bird"
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+blu = parrot("blu",10)
+woo = parrot("woo",15)
+
+print("blu is a{}".format(blu.specie))
+print("woo is also a{}".format(woo.specie))
+
+print("{} is {} years old".format(blu.name,blu.age))
+print("{} is {} years old".format(woo.name,woo.age))
