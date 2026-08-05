53d41a995a48ebba99f142732472e0ad41733834
Jaweriagul <jaweriagul2009@gmail.com>
2026-08-05 09:49:02 -0700

    py

---
+++ python/my profile card.py
@@ -0,0 +1,33 @@
+from tkinter import *
+window = Tk()
+window.title('My Profile Card')
+window.geometry('400x380')
+
+title = Label(window, text = 'My Profile Card', fg = 'white', bg = 'purple', width = 40)
+title.grid(row = 0, column = 0, columnspan = 2 , padx = 10, pady = 10)
+
+name_label = Label(window, text = 'Name:', fg = 'black', bg = 'yellow')
+name_label.grid(row = 1, column = 0, padx = 10, pady = 5)
+
+name_entry = Entry(window, fg = 'white', bg = 'green', width = 25)
+name_entry.grid(row = 1, column = 1, padx = 10, pady = 5)
+
+hobby_label = Label(window, text = 'Hobby:', fg = 'black', bg = 'yellow')
+hobby_label.grid(row = 2, column = 0, padx = 10, pady = 5)
+
+hobby_entry = Entry(window, fg = 'white', bg = 'green', width = 25)
+hobby_entry.grid(row = 2, column = 1, padx = 10, pady = 5)
+
+about_frame = Frame(window, relief = 'raised', width = 3)
+about_frame.grid(row = 3, column = 0, columnspan = 2, padx = 10, pady = 5)
+
+about_label = Label(about_frame, text = 'About Me:')
+about_label.pack()
+
+about_text = Text(about_frame, fg = 'green', bg = 'lightyellow', width = 40, height = 4)
+about_text.pack()
+
+submit = Button(window, text = 'Submit', fg = 'white', bg = 'purple', width = 20)
+submit.grid(row = 4, column = 0, columnspan = 2, padx=10, pady = 10)
+
+window.mainloop()
