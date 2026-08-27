d988a0b7d8191b7b904810db9cc7b08a5ee62263
Jaweriagul <jaweriagul2009@gmail.com>
2026-08-27 19:48:47 +0500

    py

--- python/DC/denomination calculator.py
+++ python/DC/denomination calculator.py
@@ -1,4 +1,4 @@
-from tkinter import*
+from tkinter import *
 from tkinter import messagebox
 from PIL import Image,ImageTk
 
@@ -31,11 +31,15 @@ button1= Button(root,
 button1.place(x=260, y=360)
 
 def topwin():
-    top = Toplevel
+    top = Toplevel()
     top.title('Denomination Calculator')
-    top.configure('lighg grey')
+    top.configure('light grey')
     top.geometry('600x350 + 50 + 50')
-    label = Label(top, text="Here are number of notes for denomination:", bg='light grey')
+
+    label = Label(top, text="Enter total amount:", bg='light grey')
+    entry = Entry(top)
+    lbl = Label(top, text="Here are number of notes for denomination:", bg='light grey')
+
     L1 = Label(top, text="2000", bg='light grey')
     L2 = Label(top, text="500", bg='light grey')
     L3 = Label(top, text="100", bg='light grey')
@@ -44,26 +48,26 @@ def topwin():
     t2 = Entry(top)
     t3 = Entry(top)    
 
-def calculator():
-    try:        
-        global amount
-        amount = int(entry.get())
-        note2000 = amount//2000
-        amount %= 2000
-        note500 = amount//500
-        amount %= 500
-        note100 = amount//100
-        amount %= 100
+    def calculator():
+        try:        
+            global amount
+            amount = int(entry.get())
+            note2000 = amount//2000
+            amount %= 2000
+            note500 = amount//500
+            amount %= 500
+            note100 = amount//100
+            amount %= 100
 
-        t1.delete(0,END)
-        t2.delete(0,END)
-        t3.delete(0,END)
+            t1.delete(0,END)
+            t2.delete(0,END)
+            t3.delete(0,END)
 
-        t1.insert(END,str(note2000))
-        t2.insert(END,str(note500))
-        t3.insert(END,str(note100))
-    except ValueError:
-        messagebox.showerror("Error","Please enter a valid number.")
+            t1.insert(END,str(note2000))
+            t2.insert(END,str(note500))
+            t3.insert(END,str(note100))
+        except ValueError:
+            messagebox.showerror("Error","Please enter a valid number.")
 
     btn = Button(top, text='Calculate', command=calculator, bg='brown', fg='white')
 
@@ -72,9 +76,9 @@ def calculator():
     btn.place(x=240, y=120)
     lbl.place(x=140, y=170)
 
-    l1.place(x=180, y=200)
-    l2.place(x=180, y=230)
-    l3.place(x=180, y=260)
+    L1.place(x=180, y=200)
+    L2.place(x=180, y=230)
+    L3.place(x=180, y=260)
 
     t1.place(x=270, y=200)
     t2.place(x=270, y=230)
